156281962edc1beb80435acb2747f2912b33bb13
Albert Saucedo <saucedoa7@gmail.com>
2014-11-07 23:02:14 -0600

    Initial Commit

---
+++ CustomCellPractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomCellPractice
+//
+//  Created by Albert Saucedo on 11/7/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
